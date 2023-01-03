6b0abc8de7d35ffbacc8522b68e8f7e3c03407e5
King Memole <mohsen_nassaji@yahoo.com>
2023-01-03 21:57:10 +0330

    Add Unit enum Compelet TechnoLifeProduct entity

---
+++ Domain/Enums/Unit.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Enums
+{
+    public enum Unit
+    {
+        Kilo = 1,
+        Mega = 2,
+        Giga = 3,
+        Tera = 4
+    }
+}
--- Domain/TechnoLifeProducts/TechnoLifeProduct.cs
+++ Domain/TechnoLifeProducts/TechnoLifeProduct.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Domain.Enums;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.TechnoLifeProducts
@@ -9,12 +10,35 @@ namespace Domain.TechnoLifeProducts
         [Key]
         public int Id { get; set; }
 
-        [Required, StringLength(1024)]
-        public string Link { get; set; }
-
         [Required, StringLength(250)]
         public string Name { get; set; }
 
+        [Required, StringLength(1024)]
+        public string Link { get; set; }
 
+        [StringLength(1024)]
+        public string ImageLink { get; set; }
+
+        public float? MonitorSize { get; set; }
+
+        public int? ProcessorCache { get; set; }
+
+        public Unit? ProcessorCacheStorageUnit { get; set; }
+
+        public int? Ram { get; set; }
+
+        public Unit? RamStorageUnit { get; set; }
+
+        public int? Hdd { get; set; }
+
+        public Unit? HddStorageUnit { get; set; }
+
+        public decimal? NormalPrice { get; set; }
+
+        public float? DicsountPersentage { get; set; }
+
+        public decimal? SellPrice { get; set; }
+
+        public bool IsAvailable { get; set; }
     }
 }
