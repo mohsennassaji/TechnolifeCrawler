20daf2b31da85aff82dbcb22d3e11fe90ca5ecd8
King Memole <mohsen_nassaji@yahoo.com>
2023-01-03 23:08:34 +0330

    Fix DefualtConnectionString in appsettings.json Add migration to create models Update database

---
+++ Infrastructure/Migrations/20230103193206_InitialCreate.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "base");
+
+            migrationBuilder.EnsureSchema(
+                name: "app");
+
+            migrationBuilder.CreateTable(
+                name: "GeneralSetting",
+                schema: "base",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Key = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(1024)", maxLength: 1024, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GeneralSetting", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LogManagment",
+                schema: "base",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Message = table.Column<string>(type: "nvarchar(2048)", maxLength: 2048, nullable: false),
+                    ClassName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    MethodName = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    GroupName = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    StackTrace = table.Column<string>(type: "nvarchar(max)", maxLength: 4096, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LogManagment", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Product",
+                schema: "app",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Code = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    Link = table.Column<string>(type: "nvarchar(1024)", maxLength: 1024, nullable: false),
+                    ProductType = table.Column<int>(type: "int", nullable: false),
+                    ProductProvider = table.Column<int>(type: "int", nullable: false),
+                    ImageLink = table.Column<string>(type: "nvarchar(1024)", maxLength: 1024, nullable: false),
+                    MonitorSize = table.Column<float>(type: "real", nullable: true),
+                    ProcessorCache = table.Column<int>(type: "int", nullable: true),
+                    ProcessorCacheStorageUnit = table.Column<int>(type: "int", nullable: true),
+                    Ram = table.Column<int>(type: "int", nullable: true),
+                    RamStorageUnit = table.Column<int>(type: "int", nullable: true),
+                    Hdd = table.Column<int>(type: "int", nullable: true),
+                    HddStorageUnit = table.Column<int>(type: "int", nullable: true),
+                    NormalPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    DicsountPersentage = table.Column<float>(type: "real", nullable: true),
+                    SellPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    IsAvailable = table.Column<bool>(type: "bit", nullable: false),
+                    LastUpdate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductImage",
+                schema: "app",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    ImageLink = table.Column<string>(type: "nvarchar(1024)", maxLength: 1024, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductImage", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProductImage_Product_ProductId",
+                        column: x => x.ProductId,
+                        principalSchema: "app",
+                        principalTable: "Product",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductSpecification",
+                schema: "app",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    Dimantions = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Weight = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductSpecification", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProductSpecification_Product_ProductId",
+                        column: x => x.ProductId,
+                        principalSchema: "app",
+                        principalTable: "Product",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductImage_ProductId",
+                schema: "app",
+                table: "ProductImage",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductSpecification_ProductId",
+                schema: "app",
+                table: "ProductSpecification",
+                column: "ProductId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GeneralSetting",
+                schema: "base");
+
+            migrationBuilder.DropTable(
+                name: "LogManagment",
+                schema: "base");
+
+            migrationBuilder.DropTable(
+                name: "ProductImage",
+                schema: "app");
+
+            migrationBuilder.DropTable(
+                name: "ProductSpecification",
+                schema: "app");
+
+            migrationBuilder.DropTable(
+                name: "Product",
+                schema: "app");
+        }
+    }
+}
