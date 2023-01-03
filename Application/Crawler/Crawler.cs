288b0c287562b4a13b47d12479ac6ac182bd064d
m.nassaji <mohsen_nassaji@yahoo.com>
2023-01-03 15:57:24 +0330

    Add required interfaces to crawler

--- Application/Crawler/Crawler.cs
+++ Application/Crawler/Crawler.cs
@@ -1,7 +1,18 @@
-﻿namespace Application.Crawler
+﻿using Application.TechnoLifeCrawler.PageParser;
+using Application.TechnoLifeCrawler.ProductScraper;
+
+namespace Application.Crawler
 {
     public class Crawler : ICrawler
     {
+        private readonly ITechnoLifePageParser _technoLifePageParser;
+        private readonly ITechnoLifeProductScraper _technoLifeProductScraper;
+
+        public Crawler(ITechnoLifePageParser technoLifePageParser, ITechnoLifeProductScraper technoLifeProductScraper)
+        {
+            _technoLifePageParser = technoLifePageParser;
+            _technoLifeProductScraper = technoLifeProductScraper;
+        }
         public Task Crawl(string url)
         {
             throw new NotImplementedException();
