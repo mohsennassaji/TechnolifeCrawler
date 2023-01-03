917957c780f1de25db3afdec349df1a6b92523aa
m.nassaji <mohsen_nassaji@yahoo.com>
2023-01-03 17:09:39 +0330

    Add partial data of technolife product

--- Domain/TechnoLifeProducts/TechnoLifeProduct.cs
+++ Domain/TechnoLifeProducts/TechnoLifeProduct.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.TechnoLifeProducts
 {
+    [Table("TechnoLifeProduct", Schema = "app")]
     public class TechnoLifeProduct
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, StringLength(1024)]
+        public string Link { get; set; }
+
+        [Required, StringLength(250)]
+        public string Name { get; set; }
+
+
     }
 }
--- Domain/TechnoLifeProducts/TechnoLifeProductDetail.cs
+++ Domain/TechnoLifeProducts/TechnoLifeProductDetail.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.TechnoLifeProducts
 {
+    [Table("TechnoLifeProductDetail", Schema = "app")]
     public class TechnoLifeProductDetail
     {
     }
